2732d89f7ca49d22e521d2613e7beb65eb7efa08
mikexporter <mikexporter@gmail.com>
2024-10-20 15:40:40 +0100

    latest change

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ from apache_beam.options.pipeline_options import (
     PipelineOptions,
     GoogleCloudOptions,
     SetupOptions,
+    StandardOptions,
 )
-from apache_beam.options.pipeline_options import StandardOptions
 import json
 from modules.feed import Feed
 from modules.models import Transcriber
@@ -14,7 +14,6 @@ from modules.episode import Episode
 class CustomPipelineOptions(PipelineOptions):
     @classmethod
     def _add_argparse_args(cls, parser):
-        # Add custom arguments specific to your pipeline, but NOT --runner
         parser.add_argument(
             "--local-base-path",
             default="./data",
@@ -22,54 +21,12 @@ class CustomPipelineOptions(PipelineOptions):
         )
         parser.add_argument(
             "--gcs-base-path",
-            default="gs://parallel-transcriber-dataflow-bucket/output",
             help="Google Cloud Storage base path for saving output files when running on Dataflow",
         )
         parser.add_argument(
             "--feed-url-json",
-            default="""{
-                "Fox News Hourly Update": "https://feeds.megaphone.fm/FOXM2252206613",
-                "CNN Breaking News Alerts": "https://feeds.megaphone.fm/WMHY6108281879",
-                "NPR News": "https://feeds.npr.org/500005/podcast.xml",
-                "BBC Global News Podcast": "https://podcasts.files.bbci.co.uk/p02nq0gn.rss"
-            }""",
             help="JSON string containing the feed URL dictionary",
         )
-
-
-# Retrieve options from the command line or default
-options = PipelineOptions()
-
-# Parse custom options
-custom_options = options.view_as(CustomPipelineOptions)
-
-# Access runner through StandardOptions
-standard_options = options.view_as(StandardOptions)
-
-# Check if running locally or on Dataflow
-google_cloud_options = options.view_as(GoogleCloudOptions)
-google_cloud_options.project = "parallel-audio-transcriber"
-google_cloud_options.region = "europe-west2"
-google_cloud_options.job_name = "parallel-transcriber-dataflow-job"
-google_cloud_options.staging_location = (
-    "gs://parallel-transcriber-dataflow-bucket/staging"
-)
-google_cloud_options.temp_location = "gs://parallel-transcriber-dataflow-bucket/temp"
-
-
-# Ensure the main session state is saved
-options.view_as(SetupOptions).save_main_session = True
-
-# Print the runner being used
-print(f"Runner set: {standard_options.runner}")
-
-# Determine the base path based on the runner
-if standard_options.runner == "DataflowRunner":
-    base_path = custom_options.gcs_base_path  # Cloud path for Dataflow
-else:
-    base_path = custom_options.local_base_path  # Local path for local execution
-
-print(f"Base path set to: {base_path}")
 
 
 class InitializeFeed(beam.DoFn):
@@ -97,22 +54,36 @@ class TranscribeEpisode(beam.DoFn):
         yield transcription
 
 
-def run_pipeline(pipeline_options):
-    # Parse the feed URL JSON from the pipeline options
-    feed_url_json = pipeline_options.view_as(CustomPipelineOptions).feed_url_json
-    print(feed_url_json)
+def run_pipeline(argv=None):
+    # Use argument parsing to extract runtime options
+    pipeline_options = PipelineOptions(argv)
 
-    # Convert the JSON string to a dictionary
-    if feed_url_json:
-        rss_feed_dictionary = json.loads(feed_url_json)
-    else:
+    # Parse custom options
+    custom_options = pipeline_options.view_as(CustomPipelineOptions)
+    google_cloud_options = pipeline_options.view_as(GoogleCloudOptions)
+    standard_options = pipeline_options.view_as(StandardOptions)
+
+    # Determine the base path based on the runner
+    base_path = (
+        custom_options.gcs_base_path
+        if standard_options.runner == "DataflowRunner"
+        else custom_options.local_base_path
+    )
+    print(f"Base path set to: {base_path}")
+
+    # Parse the feed URL JSON from the pipeline options
+    feed_url_json = custom_options.feed_url_json
+    if not feed_url_json:
         raise ValueError("Feed URL JSON is not provided or empty.")
+    rss_feed_dictionary = json.loads(feed_url_json)
+
     # Formatting the channel names with commas and 'and' before the last one
     channel_names = list(rss_feed_dictionary.keys())
     formatted_channel_names = (
         ", ".join(channel_names[:-1]) + " and " + channel_names[-1]
+        if len(channel_names) > 1
+        else channel_names[0]
     )
-
     print(f"Fetching latest episodes from {formatted_channel_names}")
 
     # Initialize the transcriber
@@ -127,14 +98,7 @@ def run_pipeline(pipeline_options):
                 list(rss_feed_dictionary.items())
             )  # Pass key-value pairs as (name, url)
             | "Initialize Feed" >> beam.ParDo(InitializeFeed())
-            | "Get Latest Episode"
-            >> beam.ParDo(
-                GetLatestEpisode(
-                    base_path=pipeline_options.view_as(
-                        CustomPipelineOptions
-                    ).gcs_base_path
-                )
-            )
+            | "Get Latest Episode" >> beam.ParDo(GetLatestEpisode(base_path=base_path))
             | "Transcribe Episode" >> beam.ParDo(TranscribeEpisode(transcriber))
             | "Print Results" >> beam.Map(print)
         )
@@ -143,6 +107,4 @@ def run_pipeline(pipeline_options):
 
 
 if __name__ == "__main__":
-    print("Starting Beam pipeline...")
-    run_pipeline(custom_options)
-    print("Beam pipeline finished.")
+    run_pipeline()
