66357035b5b515da90bd1fdf34cae0d75811df28
mikexporter <mikexporter@gmail.com>
2024-10-20 17:04:30 +0100

    latest change

--- build_dataflow_template.sh
+++ build_dataflow_template.sh
@@ -8,7 +8,7 @@ gcloud dataflow flex-template build "gs://parallel-transcriber-dataflow-bucket/p
   --image-gcr-path gcr.io/parallel-audio-transcriber/dataflow-transcriber-image:latest \
   --sdk-language "PYTHON" \
   --flex-template-base-image "PYTHON3" \
-  --metadata-file "metadata.json" \
   --py-path "./src" \
   --env "FLEX_TEMPLATE_PYTHON_PY_FILE=src/main.py" \
   --env "FLEX_TEMPLATE_PYTHON_REQUIREMENTS_FILE=requirements.txt"
+    #--metadata-file "metadata.json" \
